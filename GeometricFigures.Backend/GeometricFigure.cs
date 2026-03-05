0fd457ada8ff0ba72aac5bdb8c6ca16f1f2a19a8
YESSICA ORTIZ CANO <yessicaortiz1146241@correo.itm.edu.co>
2026-03-04 22:18:31 -0500

    Diagram creation Method correction in Geometric.figure

--- GeometricFigures.Backend/GeometricFigure.cs
+++ GeometricFigures.Backend/GeometricFigure.cs
@@ -7,19 +7,14 @@ namespace GeometricFigures.Backend
 {
     public abstract class GeometricFigure
     {
-        public string Name { get; set; }
-
-        public GeometricFigure(string name)
-        {
-            Name = name;
-        }
+        public string Name { get; set; } = string.Empty;
 
         public abstract double GetArea();
-        public abstract double GetPerimeter();
+        public abstract double GetPerimiter();
 
         public override string ToString()
         {
-            return $"{Name,-15} => Area.....: {GetArea(),15:N5}   Perimiter: {GetPerimeter(),15:N5}";
+            return $"{Name,-15} => Area.....: {GetArea(),15:N5}   Perimiter: {GetPerimiter(),15:N5}";
         }
     }
 }
